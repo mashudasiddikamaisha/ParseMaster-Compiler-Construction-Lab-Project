d2a6968deadad70cdd346d47ab5523bb534f6c8d
Nowshin03 <nusrathjahannowshinmu@gmail.com>
2026-07-30 22:22:09 +0600

    AST header

---
+++ src/ast/ast.h
@@ -0,0 +1,28 @@
+#ifndef AST_H
+#define AST_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct ASTNode
+{
+    char nodeType[30];      // Assignment, If, While, Identifier, etc.
+    char value[30];         // Variable name, literal value, operator
+
+    struct ASTNode *left;
+    struct ASTNode *right;
+    struct ASTNode *next;
+
+} ASTNode;
+
+/* Function declarations */
+
+ASTNode *createNode(char *nodeType, char *value,
+                    ASTNode *left, ASTNode *right);
+
+void printAST(ASTNode *root, int level);
+
+void freeAST(ASTNode *root);
+
+#endif
